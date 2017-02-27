97c1d8fdeff3fdd0791339ca0220ac187d4a8e1e
gala <kgalina.jh@gmail.com>
2017-02-27 19:44:18 +0200

    add test_simple

---
+++ Tests/test_simple.py
@@ -0,0 +1,14 @@
+import pytest
+from selenium import webdriver
+
+
+class TestOne():
+
+    def test_one(self):
+
+        self.driver = webdriver.Chrome(executable_path='/Applications/Python 3.5/chromedriver')
+        self.driver.set_window_size(1280, 1080)
+        # self.driver.maximize_window()
+
+        self.driver.get("http://voxan.su/")
+        self.driver.close()
